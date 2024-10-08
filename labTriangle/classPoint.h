c6a68607a8e5ebd3cfcc0fb4e14c0a0b69b77460
leandoero <seventeen.iinc@gmail.com>
2024-10-08 14:05:57 +0300

    /

--- labTriangle/classPoint.h
+++ labTriangle/classPoint.h
@@ -12,17 +12,17 @@ class Point {
 public:
 	Point(double first = 0.0, double second = 0.0);
 	Point(const Point& other);
-	void  moveX(double moveX);
-	void moveY(double moveY);
+	inline void  moveX(double moveX);
+	inline void moveY(double moveY);
 	double const distanceToOrigin();
 	double const distanceTo(const Point& other);
 	std::pair<double, double> ToPolar();
 	bool operator==(const Point& other);	
 	bool operator!=(const Point& other);
-	double getX() const;
-	double getY() const;
-	void setX(double x);
-	void setY(double y);
+	inline double getX() const;
+	inline double getY() const;
+	inline void setX(double x);
+	inline void setY(double y);
 	std::string printCoord() const;
 	~Point();
 };
